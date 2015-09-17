49ece3e743e1f9a3dd3e3b785d167b061c3357bb
luocn99 <luocn99@gmail.com>
2015-09-17 19:59:23 +0800

    add protobuf

--- practice/engine/rsa.cc
+++ practice/engine/rsa.cc
@@ -6,15 +6,52 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <openssl/bn.h>
-#include <openssl/rsa.h>
-#include <openssl/engine.h>
+#include "openssl/ssl.h"
 
 static int my_mod_exp(BIGNUM *r0, const BIGNUM *I, RSA *rsa, BN_CTX *ctx) {
     printf("my mod_exp\n");
     return 1;
-
 }
+
+static int my_encrypt(RSA *rsa, size_t *out_len, uint8_t *out, size_t max_out,
+        const uint8_t *in, size_t in_len, int padding)  {
+    printf("my own encrypt\n");
+    return 2;
+}
+
+static const rsa_meth_st my_rsa_meth {
+  {
+    0 /* references */,
+    1 /* is_static */,
+  },
+  NULL /* app_data */,
+
+  NULL /* init */,
+  NULL,
+
+  NULL,
+
+  NULL /* sign */,
+  NULL /* verify */,
+
+  //my_encrypt,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+
+  NULL,
+
+  //my_mod_exp /* mod_exp */,
+  NULL,
+  NULL /* bn_mod_exp */,
+
+  2 | 4,
+
+  NULL,
+  NULL,
+};
+
 
 int main(int argc, char** argv) {
     unsigned char* input_string;
@@ -28,7 +65,7 @@ int main(int argc, char** argv) {
     }
 
     ENGINE *eng = ENGINE_new();
-    if (NULL != eng) {
+    if (NULL == eng) {
         printf("fail to create ENGINE\n");
     }
 
@@ -41,16 +78,17 @@ int main(int argc, char** argv) {
     }
     strncpy((char*)input_string, argv[1], strlen(argv[1]));
 
+    ENGINE_set_RSA_method(eng, &my_rsa_meth, sizeof(RSA_METHOD));
     // Generate RSA parameters with 1024 bits (using exponent 3)
-    RSA *rsa = RSA_new();
+    RSA *rsa = RSA_new_method(eng);
     if (rsa == NULL) {
         printf("fail to alloc for rsa\n");
         exit(-1);
     }
 
-    ENGINE_set_RSA_method(eng, rsa->meth, sizeof(RSA_METHOD));
+    printf("static encrypt:%p rsa encrypt addr:%p\n", my_rsa_meth.encrypt, rsa->meth->encrypt);
     //eng->rsa_method->mod_exp = my_mod_exp;
-    eng->rsa_method  = NULL;
+//    &eng->rsa_method  = NULL;
 
     BIGNUM  *bn_e = BN_new();
     int     bits  = 1024;
@@ -74,6 +112,8 @@ int main(int argc, char** argv) {
     RSA* encrypt_rsa = RSA_new();
     encrypt_rsa->n = BN_bin2bn(n_b, n_size, NULL);
     encrypt_rsa->e = BN_bin2bn(e_b, b_size, NULL);
+    rsa->n = BN_bin2bn(n_b, n_size, NULL);
+    rsa->e = BN_bin2bn(e_b, b_size, NULL);
 
     // alloc encrypt_string
     encrypt_string = (unsigned char*)calloc(RSA_size(encrypt_rsa),
@@ -86,8 +126,10 @@ int main(int argc, char** argv) {
     // encrypt (return the size of the encrypted data)
     // note that if RSA_PKCS1_OAEP_PADDING is used,
     // flen must be < RSA_size - 41
+    printf("before public_encrypt\n");
+
     int encrypt_size = RSA_public_encrypt(strlen((char*)input_string),
-            input_string, encrypt_string, encrypt_rsa, RSA_PKCS1_OAEP_PADDING);
+            input_string, encrypt_string, rsa, RSA_PKCS1_OAEP_PADDING);
 
     // alloc decrypt_string
     decrypt_string = (unsigned char*)calloc(RSA_size(rsa),
